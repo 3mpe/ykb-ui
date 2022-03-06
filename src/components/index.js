70d1bc8bdbc97979dc739f31229e3f9560cf2c89
emre.vatansever <emre.vatansever@n11.com>
2022-03-06 09:39:35 +0300

    icon removed

--- src/components/index.js
+++ src/components/index.js
@@ -1,6 +1,5 @@
 import Header from './Header';
 import Button from './Button';
-import Icon from './Icon';
 import List from './List';
 import ListItem from './ListItem';
-export { Header, Button, Icon, List, ListItem };
+export { Header, Button, List, ListItem };
